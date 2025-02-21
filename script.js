f4fbabd1e3977d069bbb3bc59d1c249755e97162
sebbe <sebastian.gabrielsson03@gmail.com>
2025-02-21 15:50:43 +0100

    38

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
 	const today = new Date().getDay();
 	const open_time = document.querySelectorAll("li[data-day]");
-	console.log(today);
+	const text_sizes = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, input, textarea");
+	let originalSizes = {};  
+	
+	text_sizes.forEach(element => {
+		originalSizes[element] = window.getComputedStyle(element).fontSize;
+	});
+
+	document.getElementById("reset").addEventListener("click", function(){
+		text_sizes.forEach(element => {
+			if (originalSizes[element]) { 
+				element.style.fontSize = originalSizes[element]; 
+			}
+		});
+	});
+	
 	
 	open_time.forEach(item => {
 		const days = item.dataset.day.split(",");
@@ -28,6 +42,47 @@ document.addEventListener('DOMContentLoaded', function(){
 				element.style.fontSize = (current_size - 2) + "px";
 			}
 		});
+	});
+
+
+
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 100) {
+			$("#scroll_top").fadeIn();
+		}
+		else {
+			$("#scroll_top").fadeOut();
+		}
+	});
+
+	$("#scroll_top").click(function(){
+		$("html,body").animate({scrollTop: 0}, 500)
+	});
+
+
+	$("#submit_button").click(function(){
+		var name = $("#name").val();
+		var mail = $("#mail").val();
+		var number = $("#number").val();
+		var subject = $("#subject").val();
+		var message = $("#message").val();
+		let arr = [name, mail, number, subject, message]
+		var labels = ["Namn", "Mejladress", "Telefonnummer", "Ämne", "Meddelande"];
+
+		for(let i = 0; i <= arr.length; i++){
+			if(arr[i] == ""){
+				alert(labels[i] + " måste fyllas i!");
+				return false;
+			}
+		}
+
+		if(!$("#confirm").prop("checked")) {
+			alert("Du måste godkänna villkoren!")
+			return false;
+		}
+
+		alert("Formuläret skickades!");
+
 	});
 
 });
